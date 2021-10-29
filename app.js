75c7e8f4e314112bbcdfe6855e4b7f270e206016
amitanshusahu <amitansusahu@gmail.com>
2021-10-29 19:45:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+const path = require("path")
+const bodyParser = require("body-parser");
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// SETUP FOR PUG , ejs
+app.set("view engine", "pug")
+// app.set('view engine', 'ejs')
+app.set("views" , path.join(__dirname+"/views"))
+//static files
+app.use('/', express.static("public"))
+
+
+
+app.get("/", (req,res) =>{
+    res.sendFile(path.join(__dirname, "public" , "html/index.html"))
+})
+app.get("/booktickets", (req,res) =>{
+    res.status(200).sendFile(path.join(__dirname,"public","html/booking.html"))
+});
+
+
+app.get("*" , (req, res) => {
+    res.render("404.pug")
+  })
+app.listen(port, ()=>{
+    console.log("server started")
+})
